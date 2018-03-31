8f06e2158cb8d6216e58c65b50fd333c95e34670
duenboa <duenboa@163.com>
2018-03-31 11:50:35 +0800

    fix

--- BatchSqlGenerator/SqlGen.java
+++ BatchSqlGenerator/SqlGen.java
@@ -8,8 +8,8 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 /**
- * @author dengbenbo696@lu.com
- * @date 2018/3/30
+ * @author duenboa
+ * @date 2017/3/30
  */
 public class SqlGen {
 
